9340043317c156ff803721428ec16789f14b3ed5
levib9851 <Butlerl@g.crookcountyschools.org>
2021-03-06 11:19:01 -0800

    i added lab 48 html and js then completed the lab

---
+++ lab-48.js
@@ -0,0 +1,27 @@
+//let name = 'levi butler';
+//let sum = 102 * 16;
+//let divide = 1550 / 5;
+
+//console.log('name:', name);
+//console.log('sum of 102 + 16:', sum);
+//console.log('1550 / 5 is:', divide);
+
+//function partOne() {
+
+ //   console.log('name:', name);
+  //  console.log('sum of 102 + 16:', sum);
+  //  console.log('1550 / 5 is:', divide);
+
+ // }
+  
+ // partOne();
+  
+  function partTwo(name,add1,add2,numerator,denominater) {
+    
+    console.log('name:', name);
+    console.log('sum of 102 + 16:', add1 + add2);
+    console.log('1550 / 5 is:', numerator / denominater);
+
+  }
+  
+  partTwo("levi", 23, 12, 54, 3);
